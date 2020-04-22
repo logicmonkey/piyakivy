b266cb4088e3940281ffd8411eec9d09d90b8695
Piers Barber <piers.barber@logicmonkey.co.uk>
2020-04-22 22:31:24 +0100

    Piyak-CS server responds to STOP
    - also makes use of utf-8 encoding
    - and forces use of Python3 on the RPi

--- piyak-server.py
+++ piyak-server.py
@@ -6,7 +6,8 @@ from gpio_pin import gpio_pin
 
 TCP_IP = '192.168.1.104'
 TCP_PORT = 62
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+BUFFER_SIZE = 1024
+ENCODING = 'utf-8'
 
 GPIO_PIN = 2
 
@@ -16,14 +17,21 @@ pin = gpio_pin(device, GPIO_PIN)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 
-while True:
+req_str = ''
+
+while req_str != 'STOP':
     s.listen(1)
 
     conn, addr = s.accept()
     print('Connection address:', addr)
     while True:
-         data = conn.recv(BUFFER_SIZE)
-         if not data: break
+         req = conn.recv(BUFFER_SIZE)
+         req_str = req.decode(ENCODING)
+         if (not req) or (req_str == 'STOP'): break
          resp = "{}:{}".format(pin._eventcount, pin._delta)
-         conn.send(bytes(resp, 'utf-8'))
-    conn.close()
+         conn.send(bytes(resp, ENCODING))
+
+print('STOP command received')
+conn.close()
+pin.cancel()
+device.stop()
