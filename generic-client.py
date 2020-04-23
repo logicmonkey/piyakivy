071bd0fdcfbaeafdd9e3944c5490d231c5084171
Piers Barber <piers.barber@logicmonkey.co.uk>
2020-04-23 01:41:22 +0100

    Piyak-CS generic client
    - Updated to be more representative of eventual approach

--- generic-client.py
+++ generic-client.py
@@ -2,15 +2,16 @@ import socket
 
 HOST = '192.168.1.104' # Enter IP or Hostname of your server
 PORT = 62 # Pick an open Port (1000+ recommended), must match the server port
+
+ENCODING = 'utf-8'
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST,PORT))
 
-r = ' '
-while r!='x':
-    a = 0
-    while a<100:
-        s.send(bytes('ask', 'utf-8'))
-        reply = s.recv(1024)
-        print(reply.decode('utf-8'))
-        a = a+1
-    r = input("What?")
+req = ''
+while not(req=='x' or req=='STOP'):
+    req = input('>')
+    if req=='': break
+    s.send(bytes(req, ENCODING))
+    resp = s.recv(1024)
+    print(resp.decode(ENCODING))
